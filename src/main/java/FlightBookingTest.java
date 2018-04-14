4ec53a5df1f89e008b429e44f5c3a17bedf49ce6
debabratasahoo1769 <debabratasahoo.1769@gmail.com>
2018-04-14 17:57:24 +0530

    Changed id for to textbox
    id of web element for to textbox should be 'ToTag'

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -31,9 +31,11 @@ public class FlightBookingTest {
         waitFor(2000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
-
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        
+        //Web element with id toTag did not exists.
+        //id should be ToTag
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
 
